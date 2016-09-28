d8df686e9112cc05faf661be18012b8a718b85b4
suhendra_ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-09-28 10:12:04 +0800

    Add delete, update and read by id method

--- routes/bear.js
+++ routes/bear.js
@@ -3,10 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Bear = mongoose.model('Bear');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+//Routing for Get All and create new BEAR
 router.route('/')
         .post(function(req, res){
             var bear = new Bear();
@@ -21,6 +18,48 @@ router.route('/')
             });
         })
         .get(function(req, res){
+            Bear.find(function(err, data){
+                if(err)
+                    return res.send(500, err);
+                
+                return res.json(data);
+            });
+        });
 
+// Routing
+router.route('/:bearId')
+        .get(function(req, res){
+            Bear.findById(req.params.bearId, function(err, data){
+                if(err)
+                    return res.send(err);
+                return res.json(data);
+            });
+        })
+
+        // Update the bear
+        .put(function(req, res){
+            Bear.findById(req.params.bearId, function(err, bear){
+                if(err)
+                    return res.send(500, err);
+                
+                bear.name = req.body.name;
+                bear.save(function(err, data){
+                    if(err)
+                        return res.send(500, err);
+                    
+                    return res.json(data);
+                })
+            });
+        })
+
+        // Delete the bear
+        .delete(function(req, res){
+            Bear.remove({
+                _id: req.params.bearId
+            }, function(err, bear){
+                if(err)
+                    return res.send(500, err);
+                return res.json(bear);
+            });
         });
 module.exports = router;
